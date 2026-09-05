fd79bf18f5e336beb83d0e4eb21dce5427305c28
sibulelemanyela11 <sibulelemanyela2@gmail.com>
2026-09-05 07:23:22 +0200

    Create gold.js

---
+++ api/gold.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  const key = process.env.ALPHA_VANTAGE_API_KEY;
+
+  const url =
+    `https://www.alphavantage.co/query?function=GOLD_SILVER_SPOT&symbol=XAU&apikey=${key}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Market data unavailable" });
+  }
+}
